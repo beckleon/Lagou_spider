4d985c1da84520a609d3fe4a00fa8d281429551c
beckleon <beckleon@hotmail.com>
2019-07-09 14:13:59 +0800

    Update 0_test.py
    insert to MongoDB

--- 0_test.py
+++ 0_test.py
@@ -2,6 +2,7 @@
 import requests
 import time
 import random
+import pymongo
 from urllib.request import quote
 
 
@@ -19,7 +20,6 @@ def get_data(page_num, keyword):
 
 
 def main():
-    kd = "数据分析"
     for i in range(1, 31):
         print(i)
         time.sleep(random.randint(5, 10))
@@ -34,12 +34,21 @@ def main():
         # 再使用 POST 获取数据
         r = ss.post(city_api_url, headers=headers, data=get_data(i, kd), verify=False)
         print(r.json())
+        result = r.json()['content']['positionResult']['result']
+        for r in result:
+            collection.insert_one(r)
 
 
 if __name__ == '__main__':
+    client = pymongo.MongoClient(host='localhost', port=27017)
+    db = client['lagou']
+    collection = db["pc_position"]
+
+    kd = "数据分析"
+    cityname = "北京"
     city_url = "https://www.lagou.com/jobs/list_{}?city={}&cl=false&fromSearch=true&labelWords=&suginput=".format(
-        quote("数据分析"), quote("北京"))
-    city_api_url = "https://www.lagou.com/jobs/positionAjax.json?city=北京&needAddtionalResult=false"
+        quote(kd), quote(cityname))
+    city_api_url = "https://www.lagou.com/jobs/positionAjax.json?city={}&needAddtionalResult=false".format(quote(cityname))
     headers = {
         "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.119 Safari/537.36",
         "Referer": city_url,
